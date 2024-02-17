686e1084276e627dd3fe90e9fbc626e40c06b854
Ved Mishra <160348266+mved2003@users.noreply.github.com>
2024-02-17 19:37:35 +0000

    const let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =1656253
+let accountEmail = "vefdth@google.com"
+var accountPassword = "133245"
+accountCity = "jaipur"
+let accountState;                                                                                                                                                                                                                                                                               
+
+// account =2 // not allowed
+
+
+accountEmail = "sdg@jsdh.com"
+accountPassword = "212121212"
+accountCity = "Bangaluru"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
