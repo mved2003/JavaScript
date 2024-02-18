8747d75a29645c4488f98c28c4f309738ff40080
Ved Mishra <160348266+mved2003@users.noreply.github.com>
2024-02-18 18:06:07 +0000

    conbersion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = 33
+
+console.log(typeof score)
+
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33 => 33
+//"33abc" => NaN not a number 
+// true => 1; / false=> 0
+    // "" => false
+    // "ved" => true
+
+    let someNumber = 33
+
+    let stringNumber = String(someNumber)
+    console.log(stringNumber);
