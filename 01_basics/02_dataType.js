a3e08ee2d7e7eae84bcc0fc9444364d2b57d6f10
Ved Mishra <160348266+mved2003@users.noreply.github.com>
2024-02-18 18:05:34 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as newer version 
+
+// alert (3+3) // we are using nodejs,not browser 
+
+console.log(3+3)
+
+console.log("ved")
+
+let name = "ved"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+//BigInt
+// string => "ved"
+// boolean => true/false
+// null => satndalone value
+// undefined => undefined
+//Symbol => uniqe
+
